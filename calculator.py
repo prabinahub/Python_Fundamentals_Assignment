0cba025aa752a5f1dd8b47ab9c8e5ec70f3ed899
prabinahub <prabina_btechedit2023@kusoed.edu.np>
2024-11-10 08:35:15 +0545

    Task: 3 is finally completed.

--- calculator.py
+++ calculator.py
@@ -1,16 +1,43 @@
-def classify_numbers():
-    while True:
-        s = input("Enter a number (or type 'exit' if you want to quit): ")
+def calculator():
         
-        if s.lower() == "exit":
-            print("Exiting the program.")
-            break
-        num = float(s)
-        if num > 0:
-            print(f"{num} is positive.")
-        elif num< 0:
-            print(f"{num} is negative.")
-        else:
-            print("The number is zero.")
+    print("Select a operation based on your needs:")
+    print("1. Addition:")
+    print("2. Subtraction:")
+    print("3. Multiplication:")
+    print("4. Division:")
+    print("4. Modulus:")
 
-classify_numbers()
+    def add(n,n1):
+        return n+n1
+    def sub(n,n1):
+        return n-n1
+    def mult(n,n1):
+        return n*n1
+    def div(n,n1):
+        return n/n1
+    def mod(n,n1):
+        return n % n1
+    while True:
+            choice = input("Enter choice (1/2/3/4/5) or 'exit' to quit: ")
+            
+            if choice.lower() == 'exit':
+                print("Exiting the calculator.")
+                break
+            
+            if choice in ['1', '2', '3', '4','5']:
+                    num = float(input("Enter a first number: "))
+                    num1 = float(input("Enter a second number: "))
+                    
+                    if choice == '1':
+                        print(f"Result: {add(num, num1)}")
+                    elif choice == '2':
+                        print(f"Result: {sub(num, num1)}")
+                    elif choice == '3':
+                        print(f"Result: {mult(num, num1)}")
+                    elif choice == '4':
+                        print(f"Result: {div(num, num1)}")
+                    elif choice == '5':
+                        print(f"Result: {mod(num, num1)}")
+            else:
+                print("Invalid choice. Please select a valid operation.")
+calculator()
